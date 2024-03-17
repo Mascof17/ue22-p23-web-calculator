39082ea7017553b68ef516defb7299366ff0303b
Mascof17 <maxdebussac@gmail.com>
2024-03-17 11:49:54 +0100

    premeirs calculs fonctionnels

--- subject/script.js
+++ subject/script.js
@@ -10,6 +10,30 @@ const display = document.querySelector('.calculator__display');
 const numberButtons = Array.from(buttons).filter(button => !isNaN(button.textContent));
 const equalButton = Array.from(buttons).filter(button => button.textContent === '=');
 const clearButton = Array.from(buttons).filter(button => button.textContent === 'AC');
+const decimalButton = Array.from(buttons).filter(button => button.textContent === '.');
+const operatorButtons = document.querySelectorAll(".key--operator");
+
+
+var ope_1 = '';
+var ope_2 = '';
+var firstNumber = '';
+var secondNumber = '';
+var result = 'pas de calcul'
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case 'add':
+      return Number(a) + Number(b);
+    case 'substract':
+      return Number(a) - Number(b);
+    case 'multiply':
+      return Number(a) * Number(b);
+    case 'divide':
+      return Number(a) / Number(b);
+  }
+}
+
+
 
 // Add click event listener to each button
 numberButtons.forEach(button => {
@@ -24,7 +48,28 @@ numberButtons.forEach(button => {
     console.log('Button clicked:', buttonText);
     
     // Update the display with the clicked button text
-    display.textContent = buttonText;
+    
+    if (ope_1 === '') {
+      firstNumber = firstNumber + buttonText;
+      display.textContent = firstNumber;
+
+    }
+    else {
+      if (ope_2 === '') {
+        secondNumber = secondNumber + buttonText;
+        display.textContent = secondNumber;
+
+      }
+      else {
+        result = toString(operate(ope_1, firstNumber, secondNumber));
+        firstNumber = result;
+        secondNumber = secondNumber + buttonText;
+        ope_1 = ope_2;
+        ope_2 = '';
+        display.textContent = secondNumber;
+      }
+
+    }
 
   });
 });
@@ -33,21 +78,48 @@ numberButtons.forEach(button => {
 
 
 // Define a variable to store the result
-let result = 'pas de calcul';
+
 
 // Add click event listener to the equal button
-equalButton[0].addEventListener('click', () => {                
-        // Evaluate the expression and update the display with the result
-        display.textContent = result;
+equalButton[0].addEventListener('click', () => {
+  // Evaluate the expression and update the display with the result
+  display.textContent = operate(ope_1, firstNumber, secondNumber);
+  ope_1 = '';
+  ope_2 = '';
+  firstNumber = '';
+  secondNumber = '';
 });
 
 
 clearButton[0].addEventListener('click', () => {
-    display.textContent = '0';
-    result = 'pas de calcul';
+  display.textContent = '0';
+  result = 'pas de calcul';
+  ope_1 = '';
+  ope_2 = '';
+  firstNumber = '';
+  secondNumber = '';
 });
 
 
+
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    // Get the operator name from the data-action attribute
+    const clicked_button = event.target;
+    const operator = clicked_button.getAttribute('data-action');
+    // Display the operator name on the calculator display
+    if (ope_1 === '') {
+      ope_1 = operator;
+    }
+    else {
+      ope_2 = operator;
+    }
+    display.textContent = operator;
+
+    
+  });
+});
 
 
 
